7d6b15b910c7f681d765752eef770c7b6a8724c3
Abdullah Saikat Kazi <abdullah.saikat.kazi@dnb.no>
2025-05-23 09:24:40 +0200

    Add initial files

---
+++ src/com/company/Soda.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Soda
+{
+    public Soda(String name, int nr ){
+        Name = name;
+        Nr = nr;
+    }
+    public String Name;
+    public int Nr;
+
+}
